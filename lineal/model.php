cf944f038d2536fce01e9addd285a258c2f071cb
Francis Gonzales <fgonzalestello91@gmail.com>
2019-02-23 05:10:44 -0500

    Lineal data

--- lineal/model.php
+++ lineal/model.php
@@ -4,6 +4,9 @@ if (!empty($_FILES['model_json'])) {
     if (is_file(__DIR__ . '/model.json')) {
         unlink(__DIR__ . '/model.json');
     }
-
-    move_uploaded_file($_FILES["model_json"]["tmp_name"],  __DIR__ . '/model.json');
+    var_dump($_FILES['model_json']);
+    echo PHP_EOL;
+    echo move_uploaded_file($_FILES['model_json']['tmp_name'],  __DIR__ . '/model.json');
+    echo PHP_EOL;
+    echo __DIR__ . '/model.json';
 }
