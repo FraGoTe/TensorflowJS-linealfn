7042e7b704f73c2f154f4e2fdbf324fcf324396a
Francis Gonzales <fgonzalestello91@gmail.com>
2019-02-23 05:16:54 -0500

    changes

--- lineal/model.php
+++ lineal/model.php
@@ -4,10 +4,10 @@ if (!empty($_FILES['model_json'])) {
     if (is_file(__DIR__ . '/model.json')) {
         unlink(__DIR__ . '/model.json');
     }
+    if (is_file(__DIR__ . '/model.weights.bin')) {
+        unlink(__DIR__ . '/model.weights.bin');
+    }
 
-    var_dump($_FILES);
-    echo PHP_EOL;
-    echo move_uploaded_file($_FILES['model_json']['tmp_name'],  __DIR__ . '/model.json');
-    echo PHP_EOL;
-    echo __DIR__ . '/model.json';
+    move_uploaded_file($_FILES['model_json']['tmp_name'],  __DIR__ . '/model.json');
+    move_uploaded_file($_FILES['model_weights_bin']['tmp_name'],  __DIR__ . '/model.weights.bin');
 }
